5e35ad904d4b54263f52f91eec49ceadb53d376b
Sergey <sergr377@gmail.com>
2020-09-03 20:03:27 +0300

    Updates

---
+++ precache-manifest.2a0b4bd07b56459da8ee62e63d403b65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562f31929e2481f2b5ebb2b031b017a8",
+    "url": "/line-app/index.html"
+  },
+  {
+    "revision": "9f34927e64944760736c",
+    "url": "/line-app/static/css/main.f2cd8684.chunk.css"
+  },
+  {
+    "revision": "6b35d29160b5e24769a9",
+    "url": "/line-app/static/js/2.d801d2ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/line-app/static/js/2.d801d2ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f34927e64944760736c",
+    "url": "/line-app/static/js/main.2b3e64cf.chunk.js"
+  },
+  {
+    "revision": "fa52e3e8ff2e5c1d1e93",
+    "url": "/line-app/static/js/runtime-main.4f325186.js"
+  }
+]);
